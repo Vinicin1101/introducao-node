6dcae0e54a356a7ce9d8c9873c178ef135c9945a
Vinicin1101 <viniciusserafim45@gmail.com>
2021-11-12 12:11:58 -0300

    Update index.js
    +POST
    +PUT
    +DELETE

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-
 const express = require('express');
-const app = express()
+const app = express();
+
+// Body in Json
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('Olá Mundo! <a href="herois">Vejas os Heróis</a>');
 
-})
+});
 
 app.get('/oi', function (req, res){
     res.send('Olá usuário!')
     req.get()
-})
+});
 
-let herois = ["Flash", "Batman", "Homem de Ferro", "Ciborg", "Viuva-Negra"];
+let herois = ["Flash", "Batman", "Super Shock", "Ciborg", "Viuva-Negra"];
 
 // [GET] "/herois" - Read All
 app.get('/herois', function (req, res) {
-    res.send(herois)
-})
+    res.send(herois.filter(Boolean));
+});
 
 // [GET] "/herois/:" - Read Single by ID
 app.get('/herois/:id', function (req, res) {
@@ -32,7 +34,26 @@ app.get('/herois/:id', function (req, res) {
     } else {
         res.send('Solicitação inválida! Erro:NaN');
     }
-})
+});
 
-;
+// [POST] "/herois/create" - add to array
+app.post('/herois/create', function (req, res) {
+    herois.push(req.body.nome);
+    res.send('Novo herói adicionado!');
+
+});
+
+// [PUT] "/herois/update/:id" - change
+app.put('/herois/update/:id', function (req, res){
+    herois[req.params.id] = req.body.nome;
+    res.send('100% Atualizado')
+});
+
+// [] "/herois/delete" - remove from array
+app.delete('/herois/delete/:id', function (req, res) {
+   delete herois[req.params.id];
+   res.send('Removido!');
+});
+
+
 app.listen(3000);
