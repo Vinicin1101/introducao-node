92594803b1cbb505451d8de5fbcb63431f91dc17
Vinicin1101 <viniciusserafim45@gmail.com>
2021-11-12 10:54:31 -0300

    Update index.js
    +parâmetros
    +validação

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { randomInt } = require('crypto');
-const express = require('express')
+
+const express = require('express');
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Olá Mundo!');
+  res.send('Olá Mundo! <a href="herois">Vejas os Heróis</a>');
 
 })
 
@@ -14,8 +14,24 @@ app.get('/oi', function (req, res){
 
 let herois = ["Flash", "Batman", "Homem de Ferro", "Ciborg", "Viuva-Negra"];
 
+// [GET] "/herois" - Read All
 app.get('/herois', function (req, res) {
     res.send(herois)
+})
+
+// [GET] "/herois/:" - Read Single by ID
+app.get('/herois/:id', function (req, res) {
+    const id = +req.params.id;
+
+    if (Number.isInteger(id)) {
+        if(id >= 0 && id <= (herois.length - 1)){
+            res.send(herois[id]);
+        }else{
+            res.send('Out of index');
+        }
+    } else {
+        res.send('Solicitação inválida! Erro:NaN');
+    }
 })
 
 ;
