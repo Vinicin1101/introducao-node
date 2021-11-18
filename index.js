2017ae7f25097284ce9fc7ef8a61349d44e3986a
Vinicin1101 <viniciusserafim45@gmail.com>
2021-11-18 12:39:13 -0300

    Integração com MongoBD

--- index.js
+++ index.js
@@ -1,59 +1,92 @@
 const express = require('express');
 const app = express();
 
-// Body in Json
+// Definindo o corpo do requerimento como Json
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Olá Mundo! <a href="herois">Vejas os Heróis</a>');
+//    Instalar o MongoDB (npm install mongodb)
 
-});
+//    Impostar o MongoDB
+const {MongoClient, ObjectId} = require('mongodb');
 
-app.get('/oi', function (req, res){
-    res.send('Olá usuário!');
-    req.get();
-});
+//    Realizar a conexão com o o Banco de Dados
+const url = "mongodb://localhost:27017";
+const dbName =  "ocean-backend";
 
-let herois = ["Flash", "Batman", "Super Shock", "Ciborg", "Viuva-Negra"];
+// Função principal (assincrona)
+async function main() {
 
-// [GET] "/herois" - Read All
-app.get('/herois', function (req, res) {
-    res.send(herois.filter(Boolean));
-});
+    // conexão com o banco de dados
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
 
-// [GET] "/herois/:" - Read Single by ID
-app.get('/herois/:id', function (req, res) {
-    const id = +req.params.id;
+    //    Procurar pelas collection que criamos
+    const collection = db.collection('herois');
 
-    if (Number.isInteger(id)) {
-        if(id >= 0 && id <= (herois.length - 1)){
-            res.send(herois[id]);
-        }else{
-            res.send('Out of index');
-        }
-    } else {
-        res.send('Solicitação inválida! Erro:NaN');
-    }
-});
+    // Requisições
+    app.get('/', function (req, res) {
+    res.send('Olá Mundo! <a href="herois">Vejas os Heróis</a>');
 
-// [POST] "/herois/create" - add to array
-app.post('/herois/create', function (req, res) {
-    herois.push(req.body.nome);
-    res.send('Novo herói adicionado!');
+    });
 
-});
+    let herois = ["Flash", "Batman", "Super Shock", "Ciborg", "Viuva-Negra"];
 
-// [PUT] "/herois/update/:id" - change
-app.put('/herois/update/:id', function (req, res){
-    herois[req.params.id] = req.body.nome;
-    res.send('100% Atualizado')
-});
+    // [GET] "/herois" - Read All
+    app.get("/herois", async function (req, res) {
+        // Acesso ao documento
+        const documentos = await collection.find().toArray();
 
-// [] "/herois/delete" - remove from array
-app.delete('/herois/delete/:id', function (req, res) {
-   delete herois[req.params.id];
-   res.send('Removido!');
-});
+        // Resposta do requerimento
+        res.send(documentos);
+    });
+
+    // [GET] "/herois/:" - Read Single by ID
+    app.get('/herois/:id', async function (req, res) {
+        const id = +req.params.id;
+
+        // Buscando o dado com o id
+        const item = await collection.findOne({ _id: new ObjectId(id) });
+
+        // Enviando a resposta
+        res.send('Aqui está: ' + item);
+    });
+
+    // [POST] "/herois/create" - add to array
+    app.post("/herois/create", async function (req, res) {
+        const item = req.body;
+
+        await collection.insertOne(item);
+
+        res.send(item);
+    });
+
+    // [PUT] "/herois/update/:id" - change
+    app.put('/herois/update/:id', async function (req, res){
+        const id = req.params.id;
+        const item = req.body;
+
+        await collection.updateOne(
+            { _id: new ObjectId(id) },
+            {
+                $set: item
+            }
+        )
 
 
-app.listen(3000);
+        res.send('100% Atualizado')
+    });
+
+    // [] "/herois/delete" - remove from array
+    app.delete('/herois/delete/:id', async function (req, res) {
+        
+        await collection.deleteOne({_id: new ObjectId(id)})
+
+        res.send('Removido!');
+    });
+
+
+    app.listen(3000);
+}
+
+// Leitura da função principal
+main();
