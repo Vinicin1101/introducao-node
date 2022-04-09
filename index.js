ea954861b9124f01d9379760186c42ade2c67c6f
Vinicin1101 <vserafim_o@outlook.com>
2022-04-09 09:50:33 -0300

    dovenv
    corrigindo erro de vulnerabilidade de acesso ao banco de dados usando variáveis de ambiente.

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-const express = require('express');
+const express = require("express");
 
 //    Instalar o MongoDB (npm install mongodb)
 
 //    Impostar o MongoDB
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require("mongodb");
 
-//    Realizar a conexão com o o Banco de Dados
-const url = "mongodb+srv://vinicin:pQGU7fRiuhxzE523@cluster0.mt4ek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const dbName = "ocean-backend-nuvem";
+// dotenv
+require("dotenv").config();
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const COLLECTION = process.env.COLLECTION;
+const DB_NAME = process.env.DB_NAME;
+const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.dff1c.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 // Função principal (assincrona)
 async function main() {
-    // conexão com o banco de dados
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
+  // conexão com o banco de dados
+  const client = await MongoClient.connect(DB_URL);
+  const db = client.db(DB_NAME);
 
-    //    Procurar pelas collection que criamos
-    const collection = db.collection('herois');
+  //    Procurar pelas collection que criamos
+  const collection = db.collection(COLLECTION);
 
-    const app = express();
-    // Definindo o corpo do requerimento como Json
-    app.use(express.json());
+  const app = express();
+  // Definindo o corpo do requerimento como Json
+  app.use(express.json());
 
-    // Requisições
-    app.get('/', function (req, res) {
-        res.send('Olá Mundo! <a href="herois">Vejas os Heróis</a>');
+  // Requisições
+  app.get("/", function (req, res) {
+    res.send('Olá Mundo! <a href="herois">Vejas os Heróis</a>');
+  });
 
-    });
+  let herois = ["Flash", "Batman", "Super Shock", "Ciborg", "Viuva-Negra"];
 
-    let herois = ["Flash", "Batman", "Super Shock", "Ciborg", "Viuva-Negra"];
+  // [GET] "/herois" - Read All
+  app.get("/herois", async function (req, res) {
+    // Acesso ao documento
+    const documentos = await collection.find().toArray();
 
-    // [GET] "/herois" - Read All
-    app.get("/herois", async function (req, res) {
-        // Acesso ao documento
-        const documentos = await collection.find().toArray();
+    // Resposta do requerimento
+    res.send(documentos);
+  });
 
-        // Resposta do requerimento
-        res.send(documentos);
-    });
+  // [GET] "/herois/:" - Read Single by ID
+  app.get("/herois/:id", async function (req, res) {
+    const id = +req.params.id;
 
-    // [GET] "/herois/:" - Read Single by ID
-    app.get('/herois/:id', async function (req, res) {
-        const id = +req.params.id;
+    // Buscando o dado com o id
+    const item = await collection.findOne({ _id: new ObjectId(id) });
 
-        // Buscando o dado com o id
-        const item = await collection.findOne({ _id: new ObjectId(id) });
+    // Enviando a resposta
+    res.send("Aqui está: " + item);
+  });
 
-        // Enviando a resposta
-        res.send('Aqui está: ' + item);
-    });
+  // [POST] "/herois/create" - add to array
+  app.post("/herois/create", async function (req, res) {
+    const item = req.body;
 
-    // [POST] "/herois/create" - add to array
-    app.post("/herois/create", async function (req, res) {
-        const item = req.body;
+    await collection.insertOne(item);
 
-        await collection.insertOne(item);
+    res.send(item);
+  });
 
-        res.send(item);
-    });
+  // [PUT] "/herois/update/:id" - change
+  app.put("/herois/update/:id", async function (req, res) {
+    const id = req.params.id;
+    const item = req.body;
 
-    // [PUT] "/herois/update/:id" - change
-    app.put('/herois/update/:id', async function (req, res) {
-        const id = req.params.id;
-        const item = req.body;
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: item,
+      }
+    );
 
-        await collection.updateOne(
-            { _id: new ObjectId(id) },
-            {
-                $set: item
-            }
-        )
+    res.send("100% Atualizado");
+  });
 
+  // [] "/herois/delete" - remove from array
+  app.delete("/herois/delete/:id", async function (req, res) {
+    await collection.deleteOne({ _id: new ObjectId(id) });
 
-        res.send('100% Atualizado')
-    });
+    res.send("Removido!");
+  });
 
-    // [] "/herois/delete" - remove from array
-    app.delete('/herois/delete/:id', async function (req, res) {
-
-        await collection.deleteOne({ _id: new ObjectId(id) })
-
-        res.send('Removido!');
-    });
-
-
-    app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000);
 }
 
 // Leitura da função principal
